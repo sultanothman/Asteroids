c6e421c03997cb3f155c6e14634508b77efd6589
sultanothman <soltanotman@gmail.com>
2024-09-10 10:45:02 +0300

    create player handler class, draw static player sprite on the screen as a triangle

--- circleshape.py
+++ circleshape.py
@@ -4,9 +4,9 @@ import pygame
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
         if hasattr(self, "containers"):
-            super.__init__(self.containers)
+            super().__init__(self.containers)
         else:
-            super.__init__()
+            super().__init__()
         
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
--- main.py
+++ main.py
@@ -2,25 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    local_player = player.Player(x, y)
 
     while(True) :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        local_player.draw(screen)
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed / 1000
-    #print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
